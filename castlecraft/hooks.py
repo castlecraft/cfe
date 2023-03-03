9867befec57fd9baf1eb8fe96921a27ed220dcb3
Revant Nandgaonkar <revant.one@gmail.com>
2023-03-03 13:37:32 +0530

    fix: hook override openid_profile

--- castlecraft/hooks.py
+++ castlecraft/hooks.py
@@ -10,4 +10,8 @@ app_email = "support@castlecraft.in"
 app_license = "MIT"
 app_version = __version__
 
+override_whitelisted_methods = {
+    "frappe.integrations.oauth2.openid_profile": "castlecraft.services.oauth2.openid_profile"  # noqa: E501
+}
+
 auth_hooks = ["castlecraft.auth.validate"]
